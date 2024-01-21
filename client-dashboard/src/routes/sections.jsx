82fc609ff9c928181debe497b5e0dbbfe0c6fb11
Zubeda Wajid Hameed <114757482+ZubedaWajid@users.noreply.github.com>
2024-01-20 19:57:59 -0500

    ui/ux changes

--- client-dashboard/src/routes/sections.jsx
+++ client-dashboard/src/routes/sections.jsx
@@ -1,14 +1,11 @@
 import { lazy, Suspense } from 'react';
-import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+import { Outlet, useRoutes } from 'react-router-dom';
 
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
-export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
 
@@ -24,23 +21,14 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
-        { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
+        { path: 'user', element: <UserPage /> }
       ],
     },
     {
       path: 'login',
       element: <LoginPage />,
     },
-    {
-      path: '404',
-      element: <Page404 />,
-    },
-    {
-      path: '*',
-      element: <Navigate to="/404" replace />,
-    },
+   
   ]);
 
   return routes;
